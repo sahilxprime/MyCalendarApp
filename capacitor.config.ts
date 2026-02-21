dbfb299a56875cc92735d26632dc4815173e6ad4
sahilxprime <koundals52@gmail.com>
2026-02-21 18:55:52 +0530

    Rename appName from 'Calendar App' to 'Holiday 2026'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aistudio.calendarapp',
-  appName: ' Calendar App',
+  appName: ' Holiday 2026 ',
   webDir: 'dist'
 };
 
